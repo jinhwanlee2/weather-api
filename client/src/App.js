f2b88ddf3514252ece05b7c57fe144654631d447
Jin Lee <jinhwanl0880@gmail.com>
2023-07-26 00:34:52 -0700

    Base template of one city complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import search from './images/search.png'
+import rain from './images/rain.png'
+import wind from './images/wind.png'
+import humidity from './images/humidity.png'
 import './App.css';
 
 function App() {
+
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/api', {
+      method: 'GET',
+      // body: JSON.stringify({ city: city })
+    })
+    .then(
+      response => response.json()
+    ).then(
+      weatherData => {
+        setWeatherData(weatherData)
+      }
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card"> 
+      <form className="search" onSubmit={handleSubmit}> 
+        <input type="text" placeholder="enter city name" spellCheck="false" />
+        <button type="submit"><img src={search} alt="search" /></button> 
+      </form>
+      <div className="weather">
+        <img src={rain} className="weather-icon"></img>
+        <h1 className="temperature">40°F</h1>
+        <h2 className="city">Hong Kong</h2>
+        <div className="details">
+          <div className="column">
+            <img src={humidity} alt="humidity"></img>
+            <div>
+              <p className="humidity">50%</p>
+              <p>Humidity</p>
+            </div>
+          </div>
+          <div className="column">
+            <img src={wind} alt="wind"></img>
+            <div>
+              <p className="wind">40 mph</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-/*
-import React from 'react'
-
-function App() {
-  return (
-    <div>
-
-    </div>
-  )
-}
-
-export default App;
-*/
--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req, res) => {
+const apiKey = "d3dd1372f453e54dcbc1b208c92b2976";
+const apiCall = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
+app.get("/api", async (req, res) => {
+
     res.json({"users": ["userOne", "userTwo", "userThree"] })
-})
+
+    /*
+    try {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=irvine&appid=d3dd1372f453e54dcbc1b208c92b2976&units=metric');
+        const data = await response.json();
+
+        res.json(data);
+    }
+    catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Something went wrong' });
+    }*/
+});
 
 
 app.listen(5000, () => {console.log("Server started on port 5000") })
