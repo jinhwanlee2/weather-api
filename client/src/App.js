2db1f0df8c2b23a1d57070395d7871dc4ba17f6e
Jin Lee <jinhwanl0880@gmail.com>
2023-08-07 04:12:49 -0700

    weather api will now successfully updated all information every search call

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,91 @@
-import React, {useState} from 'react'
-import search from './images/search.png'
-import rain from './images/rain.png'
-import wind from './images/wind.png'
-import humidity from './images/humidity.png'
+import React, { useState } from 'react';
+import search from './images/search.png';
+import rain from './images/rain.png';
+import wind from './images/wind.png';
+import humidity from './images/humidity.png';
+import clear from './images/clear.png';
+import drizzle from './images/drizzle.png';
+import mist from './images/mist.png';
+import clouds from './images/clouds.png';
 import './App.css';
 
 function App() {
-
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+
+  // Function to get the appropriate weather icon
+  const getWeatherIcon = (weatherMain) => {
+    switch (weatherMain) {
+      case 'Clear':
+        return clear;
+      case 'Rain':
+        return rain;
+      case 'Drizzle':
+        return drizzle;
+      case 'Mist':
+        return mist;
+      case 'Clouds':
+        return clouds;
+      default:
+        return '';
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    fetch('/api', {
-      method: 'GET',
-      // body: JSON.stringify({ city: city })
-    })
-    .then(
-      response => response.json()
-    ).then(
-      weatherData => {
-        setWeatherData(weatherData)
-      }
-    )
-  }
+    fetch(`/api/${encodeURIComponent(city)}`)
+      .then((response) => response.json())
+      .then((weatherData) => {
+        setWeatherData(weatherData);
+      });
+  };
 
   return (
-    <div className="card"> 
-      <form className="search" onSubmit={handleSubmit}> 
-        <input type="text" placeholder="enter city name" spellCheck="false" />
-        <button type="submit"><img src={search} alt="search" /></button> 
+    <div className="card">
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="enter city name"
+          spellCheck="false"
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
+        <button type="submit">
+          <img src={search} alt="search" />
+        </button>
       </form>
+      <div className="error">
+        <p>Invalid city name</p>
+      </div>
       <div className="weather">
-        <img src={rain} className="weather-icon"></img>
-        <h1 className="temperature">40°F</h1>
-        <h2 className="city">Hong Kong</h2>
-        <div className="details">
-          <div className="column">
-            <img src={humidity} alt="humidity"></img>
-            <div>
-              <p className="humidity">50%</p>
-              <p>Humidity</p>
+        {weatherData && (
+          <>
+            <img
+              src={getWeatherIcon(weatherData.weather[0].main)}
+              className="weather-icon"
+              alt="Weather Icon"
+            />
+            <h1 className="temperature">{Math.round(weatherData.main.temp)}°F</h1>
+            <h2 className="city">{weatherData.name}</h2>
+            <div className="details">
+              <div className="column">
+                <img src={humidity} alt="humidity" />
+                <div>
+                  <p className="humidity">{weatherData.main.humidity}%</p>
+                  <p>Humidity</p>
+                </div>
+              </div>
+              <div className="column">
+                <img src={wind} alt="wind" />
+                <div>
+                  <p className="wind">{weatherData.wind.speed} mi/h</p>
+                  <p>Wind Speed</p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="column">
-            <img src={wind} alt="wind"></img>
-            <div>
-              <p className="wind">40 mph</p>
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
 
-const apiKey = "d3dd1372f453e54dcbc1b208c92b2976";
-const apiCall = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+// const apiKey = "d3dd1372f453e54dcbc1b208c92b2976";
+const apiCall = "https://api.openweathermap.org/data/2.5/weather?q="
+const apiKey = "&appid=d3dd1372f453e54dcbc1b208c92b2976&units=metric"
 
-app.get("/api", async (req, res) => {
-
-    res.json({"users": ["userOne", "userTwo", "userThree"] })
-
-    /*
+app.get('/api/:city', async (req, res) => {
+    const city = req.params.city;
+    const url = apiCall + city + apiKey;
+    
     try {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=irvine&appid=d3dd1372f453e54dcbc1b208c92b2976&units=metric');
-        const data = await response.json();
-
+        const response = await fetch(url);
+        var data = await response.json();
+        console.log(data);
         res.json(data);
     }
     catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Something went wrong' });
-    }*/
+    }
 });
 
 
